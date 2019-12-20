4ba6ee00395c92d93096cbf563ab2fe86a242858
saikarthik <saikarthik.b007@gmail.com>
2019-12-20 14:54:44 +0530

    adding

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('frontend'))
+app.get('/', (req, res) => res.sendFile(__dirname+'/frontend/html/home.html'))
+app.get('/login', (req, res) => res.send('Login'))
+app.get('/register', (req, res) => res.send('Register'))
+
+var student = {
+    "name":"Sai Karthik",
+    "college":"VNRVJIET",
+    "regno":"17071A1270"
+};
+
+app.get('/data',(req,res) => res.json(student))
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
